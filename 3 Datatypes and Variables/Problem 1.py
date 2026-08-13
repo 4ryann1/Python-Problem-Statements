8332c980dcb54ceb09d27f0d75213bb46ef27396
Aryan <aryanfrom2005@gmail.com>
2026-08-13 19:28:03 +0530

    2 problems of data types and variables

---
+++ 3 Datatypes and Variables/Problem 1.py
@@ -0,0 +1,8 @@
+# Create variables to store your name, age, height (in cm), and whether you are a student (True/False). Print each variable along with its data type using type().
+
+name = "Aryan"
+age = 21
+height = 173.99
+is_student = True
+
+print(f"Type of {name} is {type(name)}.\nType of {age} is {type(age)}. \nType of {height} is {type(height)}. \nHe is a student = {is_student} and the type is {type(is_student)}")
---
+++ 3 Datatypes and Variables/Problem 2.py
@@ -0,0 +1,7 @@
+# Assign the values 10, 20, and 30 to three variables a, b, and c in a single line of code. Then swap the values of a and b without using a third variable.
+
+a, b, c = 10, 20, 30
+print(a,b,c)
+
+a, b = 20, 10, 30
+print(a,b,c)
