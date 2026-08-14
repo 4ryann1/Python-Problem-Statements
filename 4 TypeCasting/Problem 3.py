843edd0d7378e9227f28d572b609de0bc5696f07
Aryan <aryanfrom2005@gmail.com>
2026-08-14 21:33:31 +0530

    p3 - age to roundoff age

---
+++ 4 TypeCasting/Problem 3.py
@@ -0,0 +1,7 @@
+# Ask the user to enter their current age using input(). Convert the input to an integer, add 1 to it, and print: "Next year you will be X years old."
+
+age = input("Enter your age: ")
+
+converted_age = int(age)
+
+print(f"Next Year you will be {converted_age + 1} years old.")
