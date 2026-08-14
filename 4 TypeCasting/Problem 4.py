0cc575e7407669ddc65e8aaf5c13f3c6b439d212
Aryan <aryanfrom2005@gmail.com>
2026-08-14 21:42:32 +0530

    p4 = tax calculation

---
+++ 4 TypeCasting/Problem 4.py
@@ -0,0 +1,6 @@
+# Given two variables price = "24.99" and tax = "2.50", cast both to floating-point numbers, calculate the total cost (price + tax), and print the result.
+
+price = 24.99
+tax = 2.50
+
+print(f"The Total Cost is: {int(price+tax)}")
