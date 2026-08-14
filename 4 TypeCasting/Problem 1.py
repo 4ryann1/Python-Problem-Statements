8b0d45a3a8f8d242c728145530be9813fad10394
Aryan <aryanfrom2005@gmail.com>
2026-08-14 21:03:17 +0530

    problem 1

---
+++ 4 TypeCasting/Problem 1.py
@@ -0,0 +1,8 @@
+# Take two numbers as input using input() (which returns strings by default) and print their sum as an integer. Handle the case where the inputs might be floats.
+
+num1 = input("Enter the Number:")
+num2 = input("Enter the Number:")
+
+sum = int(num1) + int(num2)
+
+print(f"Sum of the two Number is {sum}")
