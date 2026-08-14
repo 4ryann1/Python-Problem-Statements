70bbe131deb62ba39badaafe837263f1e5f3d658
Aryan <aryanfrom2005@gmail.com>
2026-08-14 21:48:23 +0530

    p5 updated

---
+++ 4 TypeCasting/Problem 5.py
@@ -0,0 +1,8 @@
+# Given quantity = 4 and total = 19.96, convert both variables to strings and concatenate them into the sentence: "You bought 4 items for a total of $19.96".
+
+quantity = 4
+total = 19.96
+
+quantity = str(quantity)
+total = str(total)
+print("You bought",quantity, "for a total of", total)
