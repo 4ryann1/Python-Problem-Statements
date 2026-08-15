13b13c9aca66850361c574d596ca547175cdd012
Aryan <aryanfrom2005@gmail.com>
2026-08-15 23:46:05 +0530

    problem 7

---
+++ 5 Taking User Input/Problem 7.py
@@ -0,0 +1,6 @@
+# Ask the user for their first and last name separately, then print the full name.
+
+first_name = input("Enter the First Name:")
+last_name = input("Enter the Last Name:")
+
+print(f"{first_name} {last_name}")
