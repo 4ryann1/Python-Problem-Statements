c74b45f5ff350cacf0728a72216c3c8af8cde137
Aryan <aryanfrom2005@gmail.com>
2026-08-15 23:38:02 +0530

    Problem 3.py

---
+++ 5 Taking User Input/Problem 3.py
@@ -0,0 +1,6 @@
+# Ask the user for two numbers and print their sum.
+
+num1 = int(input("Enter the Number:"))
+num2 = int(input("Enter the Number:"))
+
+print(f"The Sum of {num1} and {num2} is {num1+num2}")
